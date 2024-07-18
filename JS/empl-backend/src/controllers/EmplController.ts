ae86d873f7f5c03a3f93a53c378f0f4b18c7a70d
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-07-18 14:32:48 +0300

    chore: Refactor EmplController to use EmployeeRepository instead of EmplService

--- JS/empl-backend/src/controllers/EmplController.ts
+++ JS/empl-backend/src/controllers/EmplController.ts
@@ -1,28 +1,28 @@
 import { Request, Response } from "express";
-import { EmplService } from "../services/EmplService";
+import { EmployeeRepository } from "../repository/EmployeeRepository";
 
 export class EmplController {
 
-    private emplService: EmplService = new EmplService();
+    private emplRepository: EmployeeRepository = new EmployeeRepository();
 
     public sayHello(req:Request, res:Response){
         res.send("Hello World");
     }
 
     public async getAll(req:Request, res:Response){
-        const allEmpl = await this.emplService.getAllEmployees();
+        const allEmpl = await this.emplRepository.getAllEmployees();
         res.send(allEmpl);
     }
 
     public async getById(req:Request, res:Response){
         const id = parseInt(req.params.id);
-        const empl = await this.emplService.getEmployeeById(id);
+        const empl = await this.emplRepository.getEmployeeById(id);
         res.send(empl);
     }
 
     public async create(req:Request, res:Response){
         const employee = req.body;
-        const newEmpl = await this.emplService.createEmployee(employee);
+        const newEmpl = await this.emplRepository.createEmployee(employee);
         const newEmplId = (newEmpl as any).insertId;
         res.send(`Employee with id ${newEmplId} created`);
     }
@@ -30,7 +30,7 @@ export class EmplController {
     public async updatePosition(req:Request, res:Response){
         const id = parseInt(req.params.id);
         const position = req.body.position;
-        const updateResult = await this.emplService.updateEmployeePosition(id, position);
+        const updateResult = await this.emplRepository.updateEmployeePosition(id, position);
         if((updateResult as any).affectedRows === 0){
             res.send(`Employee with id ${id} not found`);
             return;
@@ -40,7 +40,7 @@ export class EmplController {
 
     public async delete(req:Request, res:Response){
         const id = parseInt(req.params.id);
-        const deletedEmpl = await this.emplService.deleteEmployee(id);
+        const deletedEmpl = await this.emplRepository.deleteEmployee(id);
         res.send(deletedEmpl);
     }
 
@@ -50,7 +50,7 @@ export class EmplController {
             res.send("Position query parameter is required");
             res.status(400);            
         }
-        const empl = await this.emplService.getEmployeesByPosition(position);
+        const empl = await this.emplRepository.getEmployeesByPosition(position);
         res.send(empl);
     }
 }
